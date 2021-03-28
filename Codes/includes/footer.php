48fa4e8145e0395ca539c57d1548910e0ec9639e
Riya-tech <58850956+Riya-tech@users.noreply.github.com>
2021-03-28 22:00:27 +0530

    Add files via upload

---
+++ Codes/includes/footer.php
@@ -0,0 +1,11 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+		<p class="wow zoomIn animated animated" data-wow-dela=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"></p>
+</div>
+</div>
